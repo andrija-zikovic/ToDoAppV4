03920bfa4b9d9c40e8a7643eed5a7cee4c2a84f4
andrija <andrija.zikovic@gmail.com>
2024-03-22 12:50:40 +0100

    Refactoring

--- dev-tools/storybook/tailwind.config.js
+++ dev-tools/storybook/tailwind.config.js
@@ -1,6 +1,5 @@
-export const content = [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './shared/**/src/**/*.{js,ts,jsx,tsx,mdx}',
-    './stories/**/*.{js,ts,jsx,tsx,mdx}',
-]
+import { content } from '../../tailwind.config.js'
+
+export default {
+    content: content,
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 export const content = [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './shared/**/src/**/*.{js,ts,jsx,tsx,mdx}',
-    './stories/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{html,tsx,ts',
+    './apps/**/*.{html,tsx,ts}',
+    './dev-tools/**/*.{html,tsx,ts}',
 ]
